4a5d907f04dea58dea009aff3f16102e59879f17
David Yan <davidyanduke@gmail.com>
2016-02-05 17:21:21 -0500

    Added David's Branch

---
+++ src/cellSociety/EventListener.java
@@ -0,0 +1,10 @@
+package cellSociety;
+
+/**
+ * Created by davidyan on 2/4/16.
+ */
+public interface EventListener {
+    void playAnimation();
+    void pauseAnimation();
+    void stepAnimation();
+}
