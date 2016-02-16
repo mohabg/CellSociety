9bb8d1a5c1cad41ffe0d573cdd59896855399170
David Yan <davidyanduke@gmail.com>
2016-02-16 18:49:12 -0500

    Added comments to masterpiece

--- src/View/Display.java
+++ src/View/Display.java
@@ -31,7 +31,16 @@ import src.Model.Grid;
 import java.util.*;
 /**
  * Created by davidyan on 2/1/16.
- * This entire file is part of my code masterpiece
+ * This entire file is part of my code masterpiece.
+ * I like how this class is organized in that, everything displayed for the user to see is created within this class
+ * The main Display constructor has calls to methods that create one single visual aspect of the screen
+ * Example: createButtons method creates all the buttons displayed on the screen, same goes for createSpeedSlider, among others
+ * With the code organized in this way, most of the visualization in this program is handled in a single class
+ * This code also allows for easy creation of new visual elements
+ * Example: if a user would like to create a new button, all they would have to do would be to update the createButtons method
+ * Each method is short and very easy to understand, thus allowing for easy debugging
+ * With this design, one class handles putting all the visual nodes on the screen
+ * 
  */
 public class Display {
 	private Button myPlay, myPause, myStep;
@@ -55,6 +64,7 @@ public class Display {
 		createCanvas();
 		createSpeedSlider();
 		createButtons();
+		//add elements to the grid after they are created
 		myGrid.add(myCanvas, screenRow, screenRow, 8, 5);
 		myGrid.add(myPlay, screenRow, screenColumn);
 		myGrid.add(myPause, ++screenRow, screenColumn);
@@ -62,6 +72,7 @@ public class Display {
 		myGrid.add(mySlider, screenRow+2, screenColumn, 5, 1);
 		addToRoot(myGrid);
 	}
+	
 	private void createCanvas(){
 		myCanvas = new Canvas(550,550);
 		GraphicsContext gc = myCanvas.getGraphicsContext2D();
@@ -97,6 +108,7 @@ public class Display {
 	}
 
 	public void addEventListener(EventListener listener) {
+		//add events to handle the button events
 		myPlay.setOnAction(event -> listener.playAnimation());
 		myPause.setOnAction(event ->listener.pauseAnimation());
 		myStep.setOnAction(event ->listener.stepAnimation());
@@ -117,6 +129,7 @@ public class Display {
 	}
 
 	public void makeToolbar(Group root, Stage stage){
+		//create the menu displayed on the toolbar
 		makeMenu();
 		Menu cellMenu = makeMenu();
 		Modifier myModifier = KeyCombination.META_DOWN;
@@ -131,6 +144,7 @@ public class Display {
 	}
 	
 	private Menu makeMenu(){
+		//add the list options that are actually displayed on the toolbar
 		Menu cellMenu = new Menu(myUseReader.getString("MakeMenu"));
 		newOpen = new MenuItem(myUseReader.getString("MakeOpen"));
 		newExit = new MenuItem(myUseReader.getString("MakeExit"));
@@ -139,6 +153,8 @@ public class Display {
 	}
 
 	public void makeParamSliders(List<String> listParams, EventListener listener){
+		//creates a slider for every parameter in a specific simulation
+		//with this, the user can manually change the parameters of a simulation
 		int idx = 3;
 		for(String aString: listParams){
 			Slider aSlider = createSlider(listener, aString);
@@ -162,6 +178,7 @@ public class Display {
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void drawGraph(HashMap<Integer,Integer> myMap) {
+		//creates the first instance of the graph by taking in a map that maps cell states to counts of each state
 		final CategoryAxis xAxis = new CategoryAxis();
 		final NumberAxis yAxis = new NumberAxis();
 		myGraph = new BarChart<String,Number>(xAxis,yAxis);
@@ -178,6 +195,7 @@ public class Display {
 	}
 
 	public void updateGraph(HashMap<Integer, Integer> myMap){
+		//updates the displayed graph by taking in a map that maps cell states to updated counts of each state
 		Integer toUseLength = myMap.keySet().size();
 		for(int i=0; i<toUseLength; i++){
 			if(!myMap.containsKey(i)){
